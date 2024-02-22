fbfc0d67b7b4683819d34b44e9a3513c609d6804
APPALERMO <86126373+APPALERMO@users.noreply.github.com>
2024-02-22 10:52:26 +0100

    Update script.js

--- storiagoniometria/script.js
+++ storiagoniometria/script.js
@@ -8,34 +8,35 @@ const divSin = document.querySelector(".sin")
 
 let bgImg = false // falso non c'è, vero c'è
 
-for(var i=0; i<buttons.length; i++){
-    buttons[i].addEventListener("click",(event)=>{
-        
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", (event) => {
+
         let tag = event.target.id
-        if(tag !== "partecipanti" && tag !== "editStile"){
+        if (tag !== "partecipanti" && tag !== "editStile") {
             const div = document.querySelector(`div#${tag}`)
             const localButton = document.querySelector(`button#${tag}`)
             // console.log(tag)
             div.style.display = (div.style.display === "unset") ? "none" : "unset"
             console.log(div.style.display)
-            
+
             // div.style.display = (localButton.style.transform === "unset") ? "none" : "unset"
             localButton.style.transform = (localButton.style.transform === "scale(1.5)") ? "scale(1)" : "scale(1.5)"
-        
-        }else if(tag === "partecipanti")btPartecipanti.style.display = (btPartecipanti.style.display === "unset") ? "none" : "unset"
-        else{
-            if(bgImg){
+
+        } else if (tag === "partecipanti") btPartecipanti.style.display = (btPartecipanti.style.display === "unset") ? "none" : "unset"
+        else {
+            if (bgImg) {
                 document.querySelector("body").style.background = "rgb(28, 27, 34)"
                 document.documentElement.style.setProperty("--cl", "white")
-                bgImg = false   
-            }else{
-                // document.documentElement.style.setProperty("--bg", bgImg)
-                // document.querySelector("body").style.background = "url(bg.jpg)"wwwwwwwww
-		document.querySelector("body").style.background = "white"
+                document.documentElement.style.setProperty("--bgBt", "#282c34")
+                
+                bgImg = false
+            } else {
+                document.querySelector("body").style.background = "white"
                 document.documentElement.style.setProperty("--cl", "black")
+                document.documentElement.style.setProperty("--bgBt", "#abb2bf")
                 bgImg = true
             }
         }
-        
+
     })
 }
