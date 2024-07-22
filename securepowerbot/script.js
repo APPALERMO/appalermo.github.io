6746fea80c224269b9793b240645d1b5e8a17e27
APPALERMO <86126373+APPALERMO@users.noreply.github.com>
2024-07-22 12:09:25 +0200

    Add files via upload

---
+++ securepowerbot/script.js
@@ -0,0 +1,7 @@
+const btSpegni = document.getElementById("btSpegni")
+const resoult = document.getElementById("resoult")
+
+btSpegni.addEventListener("click", () => {
+    resoult.innerText = "spegni"
+    console.log(resoult.innerText)
+})
