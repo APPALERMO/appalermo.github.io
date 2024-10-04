70f7f49f71a72a3a605c3ca6f799ff1cb543dcfe
APPALERMO <86126373+APPALERMO@users.noreply.github.com>
2024-10-04 18:06:33 +0200

    Update script.js

--- 7mezzo/multiplayer/script.js
+++ 7mezzo/multiplayer/script.js
@@ -9,8 +9,8 @@ let changeLeftBtView = true
 let counterCarteGiocatore = 0,
     counterCartePC = 0
 
-// const server = new WebSocket("ws:localhost:8080/")
-const server = new WebSocket(`ws:${location.hostname}:8080`)
+const server = new WebSocket("ws://localhost:8080/")
+// const server = new WebSocket(`ws:${location.hostname}:8080`)
 const divPlayerDate = document.getElementById("playerDate")
 const divContent = document.getElementById("contenuto")
 
