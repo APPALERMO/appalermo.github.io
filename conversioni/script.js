00d89191e0e73f499b08e8ed59dda6d1020358d0
APPALERMO <86126373+APPALERMO@users.noreply.github.com>
2024-10-13 12:34:00 +0200

    Update script.js

--- conversioni/script.js
+++ conversioni/script.js
@@ -1,6 +1,9 @@
 const result = (text) => {  document.getElementById("risultati").innerHTML = text  }
 const addResult = (text) => {  document.getElementById("risultati").innerHTML += text  }
 
+let last_base,
+    last_conver
+
 const convert_to_ab = (number,base) => {  
     const n = Number(number).toString(parseInt(base)).toUpperCase()
     
@@ -19,7 +22,7 @@ const convert_to_ab = (number,base) => {
                 n2[i] = n2[i].padStart(8,"0")
             }
             
-            for(let i = 0; i<n2.length; i++){
+            for(let i = 1; i<n2.length; i++){
                 n2[i] = n2[i].split("").reverse().join("")
             }
             n2 = n2.join(" ")
@@ -66,7 +69,9 @@ const number_to = (number,base) => {
             }
         }
         n_d += parseInt((s[i])*b**i)
-        pg.push(` ${(s[i])}*${b}<sup>${i}</sup>`)
+        if(s[i] != 0){
+            pg.push(` ${(s[i])}*${b}<sup>${i}</sup>`)
+        }
     }
     return [n_d, pg.reverse()]  
 
@@ -116,64 +121,72 @@ const convert_to = (number, base) => {
 }
 
 function confirm(){
-    
     let view_procedure = document.querySelector('input[name="view_procedure"]:checked')
     
     let input = document.getElementById("inputNumber").value
-    let convert = document.querySelector('input[name="convert_to"]:checked').value
     let base = document.querySelector('input[name="base"]:checked').value
+    let convert = document.querySelector('input[name="convert_to"]:checked').value
     
-    if(base == 16) 
-        document.getElementById("inputNumber").setAttribute("type", "text")
-    else
-        document.getElementById("inputNumber").setAttribute("type", "number")
+    if(base == last_conver || base == convert) {
+            document.getElementById(`c${last_base}`).checked = true
+    }    
+    
+    if(input){
         
-    
-    if(parseInt(base) === 10){
-        if(convert === "allBase") result(`Binario: ${convert_to_ab(input,2)}<br>Ottale: ${convert_to_ab(input,8)}<br>Esadecimale: ${convert_to_ab(input,16)}`)
-        else {
-            convert = parseInt(convert)
-            var pg = convert_to(input,convert)
-            result("")
-            if(view_procedure){
-            
-                for(var i=0; i<pg.length; i++){
-                    addResult(`${pg[i][0]} | ${pg[i][1]} <br>`)
+        if(base == 16) 
+            document.getElementById("inputNumber").setAttribute("type", "text")
+        else
+            document.getElementById("inputNumber").setAttribute("type", "number")
+        
+        
+        if(parseInt(base) === 10){
+            if(convert === "allBase") result(`Binario: ${convert_to_ab(input,2)}<br>Ottale: ${convert_to_ab(input,8)}<br>Esadecimale: ${convert_to_ab(input,16)}`)
+            else {
+                convert = parseInt(convert)
+                var pg = convert_to(input,convert)
+                result("")
+                if(view_procedure){
+                
+                    for(var i=0; i<pg.length; i++){
+                        addResult(`${pg[i][0]} | ${pg[i][1]} <br>`)
+                    }
                 }
+                addResult("<hr>")
+                addResult(convert_to_ab(input,convert))
+                
             }
-            addResult("<hr>")
-            addResult(convert_to_ab(input,convert))
-            
-        }
-    }else{
-        base = parseInt(base)
-        if(parseInt(convert) === 10){
-            var pg = number_to(input, base)
-            // result(`<hr/>${pg[1]} = ${pg[0]}`)
-            result("")
-            addResult("<hr/>")
-            if(view_procedure) {
-                for(var i=0; i<pg[1].length; i++){
-                    addResult(` + ${pg[1][i]}`)
-                }
-            } 
-            addResult(` = ${pg[0]}`)
         }else{
-            var pg = convert_to_ab(number_to(input, base)[0], convert)
-            result(`<hr/> ${pg}`)
+            base = parseInt(base)
+            if(parseInt(convert) === 10){
+                var pg = number_to(input, base)
+                // result(`<hr/>${pg[1]} = ${pg[0]}`)
+                result("")
+                addResult("<hr/>")
+                if(view_procedure) {
+                    for(var i=0; i<pg[1].length; i++){
+                        addResult(` + ${pg[1][i]}`)
+                    }
+                } 
+                addResult(` = ${pg[0]}`)
+            }else{
+                var pg = convert_to_ab(number_to(input, base)[0], convert)
+                result(`<hr/> ${pg}`)
+            }
         }
+        
     }
+    last_base = base
+    last_conver = convert
     
 }
 
 document.getElementById("inputNumber").addEventListener("input", confirm)
 
-window.onload = ()=>{
+window.onload = () => {
     // console.log(document.querySelectorAll("#base"))
-    for(var i=0; i<5; i++){
-        document.querySelectorAll("#convert_to")[i].addEventListener("click",confirm)
-        try {document.querySelectorAll("#base")[i].addEventListener("click",confirm)}
-        catch{}
+    for(var i=0; i<4; i++){
+        document.querySelectorAll("input[name='base']")[i].addEventListener("click",confirm)
+        document.querySelectorAll("input[name='convert_to']")[i].addEventListener("click",confirm)
     }
     
     // per dispositivi mobile
@@ -182,5 +195,5 @@ window.onload = ()=>{
         
         contenuto.style.transform = "scale(1.4) translate(0,15%)"
     }
-    
+    confirm()
 } 
